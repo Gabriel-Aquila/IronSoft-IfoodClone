c9c9908b3d5b1f6ba83d4516dfd47c3145facaa3
aquila_omago <gabrielaquila15@gmail.com>
2024-03-06 17:27:52 -0300

    IronSoft_IfoodClone- Configuração do jwt-decode

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 5500;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
